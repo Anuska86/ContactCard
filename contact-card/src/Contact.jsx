d2ab2da20f4647f12c61a2ca933010ae49317735
Anuska86 <91799755+Anuska86@users.noreply.github.com>
2025-01-15 16:46:24 +0100

    Header

--- contact-card/src/Contact.jsx
+++ contact-card/src/Contact.jsx
@@ -1,25 +1,25 @@
+import matty from "../src/imgs/matty.png";
+import phoneIcon from "../src/imgs/phoneIcon.png";
+import emailLogo from "../src/imgs/emailLogo.png";
+
 export default function Contact() {
-    return (
-        <article className="contact-card">
-            <img
-                src="./imgs/Matty.png"
-                alt="Photo of an adorable cat"
-            />
-            <h3>Mr. <Matty></Matty></h3>
-            <div className="info-group">
-                <img
-                    src="./images/phone-icon.png"
-                    alt="phone icon"
-                />
-                <p>(212) 555-1234</p>
-            </div>
-            <div className="info-group">
-                <img
-                    src="./images/mail-icon.png"
-                    alt="mail icon"
-                />
-                <p>mr.whiskaz@catnap.meow</p>
-            </div>
-        </article>
-    )
+  return (
+    <div>
+      <header>
+        <h1>Contact Card</h1>
+      </header>
+      <article className="contact-card">
+        <img className="matty" src={matty} alt="Photo of an adorable cat" />
+        <h3>Mr. Matty</h3>
+        <div className="info-group">
+          <img className="phone" src={phoneIcon} alt="Phone icon" />
+          <p>(212) 555-1234</p>
+        </div>
+        <div className="info-group">
+          <img className="email" src={emailLogo} alt="Email logo" />
+          <p>mr.matty@catnap.meow</p>
+        </div>
+      </article>
+    </div>
+  );
 }
